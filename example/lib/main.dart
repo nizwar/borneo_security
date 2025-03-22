1a495d127138a6a02aece6e09967c4f2d714fd90
Mochamad Nizwar Syafuan <nizwar_id@icloud.com>
2025-03-22 23:36:54 +0700

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:borneo_security/borneo_security.dart';
+import 'package:borneo_security/model/app_package.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ndialog/ndialog.dart';
@@ -7,8 +8,32 @@ void main(List<String> args) {
   runApp(MaterialApp(home: const App()));
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  BorneoPlayIntegrity integrityChecker = BorneoPlayIntegrity();
+  BorneoMockLocationSecurity mockChecker = BorneoMockLocationSecurity();
+  BorneoPackages packages = BorneoPackages();
+
+  List<AppPackage> apps = [];
+
+  @override
+  void initState() {
+    integrityChecker.initialize(0).catchError((e) => false).then((value) {
+      print("Integrity initialized: $value");
+    });
+    packages.getInstalledApps(true).then((value) {
+      setState(() {
+        apps = value;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,41 +45,21 @@ class App extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () async {
-                var mockChecker = BorneoMockLocationSecurity();
-                var packages = BorneoPackages();
-                final mockedApps = await packages.getInstalledApps().then(
-                  (value) {
-                    return value
-                        .where((item) => item.permissions
-                            .map((item) => item.toLowerCase())
-                            .contains(
-                                "android.permission.access_mock_location"))
-                        .map((item) => item.packageName)
-                        .toList();
-                  },
-                ).showCustomProgressDialog(context);
-                final hasMockedApps = mockedApps?.isNotEmpty ?? false;
-                final isMockSettingsEnabled =
-                    await mockChecker.isMockEnabled().catchError((e) => false);
+                final mockedApps =
+                    apps.where((item) => item.permissions.map((item) => item.toLowerCase()).contains("android.permission.access_mock_location")).map((item) => item.packageName).toList();
+
+                final hasMockedApps = mockedApps.isNotEmpty;
+                final isMockSettingsEnabled = await mockChecker.isMockEnabled().catchError((e) => false);
 
                 showDialog(
-                  // ignore: use_build_context_synchronously
                   context: context,
                   builder: (context) => AlertDialog(
                     title: const Text('Mocked Apps'),
                     content: Column(
                       mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text('Has Mocked Apps: $hasMockedApps'),
-                        Text('Mocked Apps:\n${mockedApps?.join('\n')}'),
-                        Text('Is Mock Settings Enabled: $isMockSettingsEnabled')
-                      ],
+                      children: [Text('Has Mocked Apps: $hasMockedApps'), Text('Mocked Apps:\n${mockedApps.join('\n')}'), Text('Is Mock Settings Enabled: $isMockSettingsEnabled')],
                     ),
-                    actions: [
-                      TextButton(
-                          onPressed: () => Navigator.of(context).pop(),
-                          child: const Text('Close'))
-                    ],
+                    actions: [TextButton(onPressed: () => Navigator.of(context).pop(), child: const Text('Close'))],
                   ),
                 );
               },
@@ -62,72 +67,39 @@ class App extends StatelessWidget {
             ),
             ElevatedButton(
               onPressed: () async {
-                var integrityChecker = BorneoPlayIntegrity();
-                //Write your own product id and nonce here
-                await integrityChecker.initialize(0).catchError((_) => false);
-                final integrityToken = await integrityChecker
-                    .getIntegrityToken()
-                    .showCustomProgressDialog(context);
+                final integrityToken = await integrityChecker.getIntegrityToken().showCustomProgressDialog(context);
                 AlertDialog(
                   title: Text("Play Integrity Token"),
                   content: InkWell(
                     onTap: () {
-                      Clipboard.setData(
-                          ClipboardData(text: integrityToken ?? "No token"));
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text("Copied to clipboard")));
+                      Clipboard.setData(ClipboardData(text: integrityToken!));
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Copied to clipboard")));
                       Navigator.pop(context);
                     },
-                    child: Padding(
-                        padding: EdgeInsets.all(10),
-                        child: SingleChildScrollView(
-                            child: Text(integrityToken ?? "No token"))),
+                    child: Padding(padding: EdgeInsets.all(10), child: SingleChildScrollView(child: Text(integrityToken ?? "No token"))),
                   ),
                 ).show(context);
               },
               child: const Text('Check Integrity'),
             ),
             Expanded(
-              child: FutureBuilder(
-                future: BorneoPackages().getInstalledApps(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
+              child: Builder(
+                builder: (context) {
+                  if (apps.isNotEmpty) {
                     return ListView(
-                        children: snapshot.data!
+                        children: apps
                             .map(
                               (item) => ListTile(
                                 title: Text(item.name),
                                 subtitle: Text(item.packageName),
-                                leading: FutureBuilder(
-                                  future: BorneoPackages()
-                                      .getIcon(item.packageName),
-                                  builder: (context, snapshot) {
-                                    if (snapshot.hasData) {
-                                      return Image.memory(snapshot.data!);
-                                    } else {
-                                      return SizedBox.shrink();
-                                    }
-                                  },
-                                ),
                                 onTap: () {
                                   AlertDialog(
                                     title: Text("${item.name} Information"),
                                     content: SingleChildScrollView(
                                       child: Column(
                                         mainAxisSize: MainAxisSize.min,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.stretch,
-                                        children: item
-                                            .toMap()
-                                            .entries
-                                            .map(
-                                              (e) => ListTile(
-                                                title: Text(e.key),
-                                                subtitle:
-                                                    Text(e.value.toString()),
-                                              ),
-                                            )
-                                            .toList(),
+                                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                                        children: item.toMap().entries.map((e) => ListTile(title: Text(e.key), subtitle: Text(e.value.toString()))).toList(),
                                       ),
                                     ),
                                   ).show(context);
@@ -136,10 +108,7 @@ class App extends StatelessWidget {
                             )
                             .toList());
                   } else {
-                    return Container(
-                        height: 300,
-                        alignment: Alignment.center,
-                        child: CircularProgressIndicator());
+                    return Container(height: 300, alignment: Alignment.center, child: CircularProgressIndicator());
                   }
                 },
               ),
